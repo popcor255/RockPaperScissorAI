94abd53c058642a795dfffda6e7a0dc7b6759742
popcor255 <popcor255@gmail.com>
2019-10-25 22:40:59 -0400

    first commit

---
+++ source/js/soundAI.js
@@ -0,0 +1,17 @@
+if (annyang) {
+    // Let's define our first command. First the text we expect, and then the function it should call
+    var commands = {
+      'rock paper scissor shoot': function() {
+        var computer_move = document.getElementById("computer_move");
+        computer_move.innerText = randomMove();
+        updateScoreboard();
+        console.log("moved made!");
+      }
+    };
+  
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+  
+    // Start listening. You can call this here, or attach this call to an event, button, etc.
+    annyang.start();
+}
---
+++ source/js/videoAI.js
@@ -0,0 +1,40 @@
+var video;
+var currentPrediction;
+var classifier;
+
+function setup(){
+    var width  = 640;
+    var height = 480;
+    var x = ((windowWidth - width) / 2);
+    var y = (windowHeight - height) / 2;
+    var cnv = createCanvas(width, height);
+
+    background(51);
+    video = createCapture(VIDEO);
+    classifier = ml5.imageClassifier('MobileNet', video, modelReady);
+    video.hide();
+    cnv.position(x, 0);
+}
+
+function draw(){
+    image(video, 0, 0);
+}
+
+function modelReady() {
+  console.log('Model is ready!!!');
+  classifier.predict(gotResults);
+}
+  
+function gotResults(error, results) {
+   
+    if (error) {
+      console.error(error);
+    } else {
+      //console.log(results);
+      label = results[0].label;
+      document.getElementById("player_move").innerText = label;
+      classifier.predict(gotResults);
+    }
+
+}
+  
