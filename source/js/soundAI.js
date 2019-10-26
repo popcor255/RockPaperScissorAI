54150c0fd5559472cecf3654a0794f1c5f00ccb4
popcor255 <popcor255@gmail.com>
2019-10-25 23:55:36 -0400

    update to https

--- source/js/soundAI.js
+++ source/js/soundAI.js
@@ -1,20 +1,18 @@
 
-window.onload = function (){
-  if (annyang) {
-      // Let's define our first command. First the text we expect, and then the function it should call
-      var commands = {
-        'rock paper scissor shoot': function() {
-          var computer_move = document.getElementById("computer_move");
-          computer_move.innerText = randomMove();
-          updateScoreboard();
-          console.log("moved made!");
-        }
-      };
-    
-      // Add our commands to annyang
-      annyang.addCommands(commands);
-    
-      // Start listening. You can call this here, or attach this call to an event, button, etc.
-      annyang.start();
-  }
+if (annyang) {
+  // Let's define our first command. First the text we expect, and then the function it should call
+  var commands = {
+    "rock paper scissor shoot": function() {
+      var computer_move = document.getElementById("computer_move");
+      computer_move.innerText = randomMove();
+      updateScoreboard();
+      console.log("moved made!");
+    }
+  };
+
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start();
 }
