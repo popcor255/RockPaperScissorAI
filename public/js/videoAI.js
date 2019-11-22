8639f2321191e35ece18843eb8a1bc1a6fd9b620
“popcor255” <popcor255@gmail.com>
2019-11-22 12:25:38 -0500

    implemented videoAI

--- public/js/videoAI.js
+++ public/js/videoAI.js
@@ -1,46 +1,65 @@
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/H5UH_cjV/';
+
+// Video
 var video;
-var label = 'loading model';
-var currentPrediction;
-var classifier;
+var flippedVideo;
+// To store the classification
+var label = "";
 
-function setup(){
-    var width  = 640;
-    var height = 480;
-    var x = ((windowWidth - width) / 2);
-    var y = (windowHeight - height) / 2;
-    var cnv = createCanvas(width, height);
-
-    background(51);
-    video = createCapture(VIDEO);
-    //classifier = ml5.imageClassifier('MobileNet', video, modelReady);
-    video.hide();
-    cnv.position(x, 0);
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
 }
 
-function draw(){
-    image(video, 0, 0);
+function setup() {
+  var width  = 640;
+  var height = 480;
+  var x = ((windowWidth - width) / 2);
+  var y = (windowHeight - height) / 2;
+  var cnv = createCanvas(width, height);
+  cnv.position(x, 0);
+
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  // flippedVideo = ml5.flipImage(video);
+  // Start classifying
+  classifyVideo();
 }
 
-function modelReady() {
-  console.log('Model is ready!!!');
-  classifier.load('model.json', customModelReady);
-  classifier.predict(gotResults);
+function draw() {
+  background(0);
+  // Draw the video
+  image(flippedVideo, 0, 0);
+
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
 }
 
-function customModelReady(){
-  console.log('Custom Model is ready!!!');
-  label = 'model ready';
-  classifier.classify(gotResults);
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
 }
 
-function gotResults(error, results) {
-
-    if (error) {
-      console.error(error);
-    } else {
-      //console.log(results);
-      label = results[0].label;
-      classifier.predict(gotResults);
-    }
-
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  // Classifiy again!
+  classifyVideo();
 }
